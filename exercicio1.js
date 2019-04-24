ecfa9cd2ec97eff070f2f4fd9b1359006da59563
liberioafonso <liberioafonso@gmail.com>
2019-04-24 19:03:01 -0300

    Commit Final

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,11 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+let botoes = document.querySelectorAll(".botao-expandir-retrair");
+
+for (let i = 0; i < botoes.length; i++) {   
+	botoes[i].addEventListener('click', function(e) {
+    let pn = botoes[i].parentNode
+    pn.classList.toggle('expandido')
+    botoes[i].innerHTML = botoes[i].innerHTML === '+' ? '-' : '+';
+  })
+}
--- exercicio2.js
+++ exercicio2.js
@@ -1,11 +1,23 @@
 // Faça o exercício da galeria de imagens aqui
 // Este arquivo ainda não está incluído no arquivo HTML
-
-const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
-  todasAsImagens = [
+const server_imagens = 'https://fegemo.github.io/cefet-web/images/',
+  imagens = [
     'philae-parts.jpg',
     'philae-rosetta.jpg',
     'philae-separation.jpg',
     'philae-67-picture.jpg',
     'philae-collecting.jpg'
   ];
+
+  let i = 0;
+  const prox = document.querySelector("#proximo");
+  const ant = document.querySelector("#anterior");
+  let slide = document.querySelector("#slide");
+  prox.addEventListener("click", function(e) {
+    i = (i + 1) % imagens.length;
+    slide.src = server_imagens + imagens[i];
+  });
+  ant.addEventListener("click", function(e) {
+    i = i === 0 ? 4 : (i - 1)
+    slide.src = server_imagens + imagens[i];
+  });
